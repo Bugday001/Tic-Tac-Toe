541ed753f77641f4e2619b8e9a0a053143d81c10
unknown <1924388412@qq.com>
2021-11-01 21:22:41 +0800

    first

---
+++ TicTacToe_game.py
@@ -0,0 +1,156 @@
+
+
+class TicTacToe:
+
+    def __init__(self):
+        self.cell_num = 3  # 格子数
+        self.g_map = [[0 for y in range(self.cell_num)] for x in range(self.cell_num)]
+        self.t_map = self.g_map.copy()  # 临时棋盘，求解回报值
+        self.cur_step = 0  # 步数
+        self.chess = []
+        self.full = 0  # 是否下满了
+        self.player = 1  # -1是电脑，1是人类
+        self.best_loc = [0, 0]  # 电脑下棋的最佳位置
+
+    # 玩家落子
+    def move(self, pos_x, pos_y):
+        self.player = -1  # 下次电脑下棋
+        if 0 <= pos_x <= self.cell_num - 1 and 0 <= pos_y <= self.cell_num - 1:  # 判断能否落子
+            if self.g_map[pos_x][pos_y] == 0:
+                self.g_map[pos_x][pos_y] = 1
+                self.cur_step += 1
+                self.chess.append((pos_x, pos_y, 1))
+                return 1
+        else:
+            return 0
+
+    # 判断游戏结局。0进行中，1玩家胜，-1电脑胜利，2平局
+    def game_result(self):
+        # 判断是否横向三子
+        count = 0
+        for y in range(self.cell_num):
+            count = 0
+            for x in range(self.cell_num):
+                count += self.g_map[x][y]
+            if count % 3 == 0 and count != 0:
+                return count / 3
+        # 判断是否纵向三子
+        for x in range(self.cell_num):
+            count = 0
+            for y in range(self.cell_num):
+                count += self.g_map[x][y]
+            if count % 3 == 0 and count != 0:
+                return count / 3
+        # 判断是否左上-右下三子
+        count = 0
+        count += self.g_map[0][0]+self.g_map[1][1]+self.g_map[2][2]
+        if count % 3 == 0 and count != 0:
+            return count / 3
+        # 判断是否右上-左下三子
+        count = 0
+        count += self.g_map[0][2] + self.g_map[1][1] + self.g_map[2][0]
+        if count % 3 == 0 and count != 0:
+            return count / 3
+        # 判和
+        if self.full:
+            return 2
+        return 0
+
+    # 电脑下棋：-1
+    def ai_move(self):
+        self.maxmin_search(9-self.cur_step)
+        self.player = 1  # 下次人类下棋
+        self.g_map[self.best_loc[0]][self.best_loc[1]] = -1
+        self.cur_step += 1
+        self.chess.append((self.best_loc[0], self.best_loc[1], 2))
+        return self.best_loc[0], self.best_loc[1]
+
+        # self.full = 1
+        # return -1, -1  # 满了
+
+    def logic(self):
+        pass
+
+    # 合法的落棋位置集合
+    def legal_loc(self):
+        left_loc = []
+        for i in range(self.cell_num):
+            for j in range(self.cell_num):
+                if self.g_map[i][j] == 0:
+                    left_loc.append((i,j))
+        return left_loc
+
+    # 落棋
+    def move_piece(self, loc):
+        self.g_map[loc[0]][loc[1]] = self.player
+        self.player *= -1  # 切换下次的player
+
+    # 悔棋
+    def remove_piece(self, loc):
+        self.g_map[loc[0]][loc[1]] = 0
+        self.player *= -1  # 切换上次的player
+
+    # 评分
+    def each_value(self, player):
+        # 填满棋子
+        for i in range(self.cell_num):
+            for j in range(self.cell_num):
+                if self.g_map[i][j] != 0:
+                    self.t_map[i][j] = self.g_map[i][j]
+                else:
+                    self.t_map[i][j] = player
+        # 评分
+        count = 0
+        for y in range(self.cell_num):
+            for x in range(self.cell_num):
+                count += self.t_map[x][y]
+            count = int(count / 3)
+        for x in range(self.cell_num):
+            for y in range(self.cell_num):
+                count += self.t_map[x][y]
+            count = int(count / 3)
+        # 斜的
+        count += int((self.t_map[0][0] + self.t_map[1][1] + self.t_map[2][2]) / 3)
+        count += int((self.t_map[0][2] + self.t_map[1][1] + self.t_map[2][0]) / 3)
+        return count
+
+    def Evaluate(self):
+        if self.game_result() == -1:
+            return 999
+        elif self.game_result() == 1:
+            return -999
+        # 电脑评分
+        ai_value = self.each_value(-1)
+        # 人类评分
+        man_value = self.each_value(1)
+        return ai_value + man_value
+
+    def maxmin_search(self, depth):
+        if -1 == self.game_result() or 1 == self.game_result():
+            return self.Evaluate()
+        if depth == 0:
+            return self.Evaluate()
+        if self.player == -1:
+            best_value = -999
+        elif self.player == 1:
+            best_value = 999
+        # 得到空的位置
+        left_loc = self.legal_loc()
+        for loc in left_loc:
+            self.move_piece(loc)
+            value = self.maxmin_search(depth-1)
+            self.remove_piece(loc)
+            if self.player == 1:
+                if value < best_value:
+                    best_value = value
+                    if depth == 9-self.cur_step:
+                        self.best_loc = loc
+            elif self.player == -1:
+                if value > best_value:
+                    best_value = value
+                    if depth == 9 - self.cur_step:
+                        self.best_loc = loc
+        return best_value
+
+
+
---
+++ game_run.py
@@ -0,0 +1,108 @@
+from TicTacToe_game import TicTacToe
+import sys
+import pygame
+from pygame.locals import *
+
+pygame.init()
+# 绘制棋盘
+space = 60  # 四周留下的边距
+cell_size = 40  # 每个格子大小
+cell_num = 4
+grid_size = cell_size * (cell_num - 1) + space * 2  # 棋盘的大小
+screen_caption = pygame.display.set_caption('井字棋')
+screen = pygame.display.set_mode((grid_size, grid_size))  # 设置窗口长宽
+# 结果字体与restart按钮
+font1 = pygame.font.Font("font/font.ttf", 24)
+font2 = pygame.font.Font("font/font.ttf", 20)
+font_position = [(55, 0), (85, 200)]
+white = (255, 255, 255)
+black = (30, 30, 30)
+running = True
+an = 0  # 结果
+ch = 0
+
+
+# 人类下棋及控制
+def man_move():
+    global ch, an, g
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        # restart game
+        elif event.type == MOUSEBUTTONUP:
+            x, y = pygame.mouse.get_pos()
+            if an:
+                if font_position[1][0] <= x <= font_position[1][0] + 50 and font_position[1][1] <= y <= \
+                        font_position[1][1] + 30:
+                    an = 0
+                    # 删除原来的数据，重新建立棋盘数据
+                    del g
+                    g = TicTacToe()
+            else:
+                xi = int(round((x - space - cell_size / 2) * 1.0 / cell_size))  # 获取到x方向上取整的序号
+                yi = int(round((y - space - cell_size / 2) * 1.0 / cell_size))  # 获取到y方向上取整的序号
+                ch = g.move(xi, yi)
+
+
+# 绘制出棋子位置
+def refresh_board():
+    global ch, an
+    screen.fill((128, 128, 128))  # 将界面设置为蓝色
+    # 画边界
+    for x in range(0, cell_size * cell_num, cell_size):
+        pygame.draw.line(screen, (200, 200, 200), (x + space, 0 + space),
+                         (x + space, cell_size * (cell_num - 1) + space), 1)
+    for y in range(0, cell_size * cell_num, cell_size):
+        pygame.draw.line(screen, (200, 200, 200), (0 + space, y + space),
+                         (cell_size * (cell_num - 1) + space, y + space), 1)
+    # 画棋子
+    for x, y, player in g.chess:
+        piece_location = [int((x + 0.5) * cell_size) + space, int((0.5 + y) * cell_size) + space]
+        if player == 1:
+            pygame.draw.circle(screen, white, piece_location, 16, 16)
+        else:
+            pygame.draw.circle(screen, black, piece_location, 16, 16)
+    # 判结果人类获胜
+    if an == 1:
+        text = font1.render("VICTORY!!", True, white)
+        text2 = font2.render("RESTART", True, white)
+        screen.blit(text, (font_position[0][0]+10, font_position[0][1]))
+        screen.blit(text2, font_position[1])
+        ch = 0
+    # 判结果电脑获胜
+    elif an == -1:
+        text1 = font1.render("GAME OVER!", True, white)
+        text2 = font2.render("RETRY", True, white)
+        screen.blit(text1, font_position[0])
+        screen.blit(text2, font_position[1])
+
+    elif g.cur_step == 10:
+        text1 = font1.render("Stalemate!", True, white)
+        text2 = font2.render("RETRY", True, white)
+        screen.blit(text1, (font_position[0][0]+10, font_position[0][1]))
+        screen.blit(text2, font_position[1])
+        an = 2
+
+
+def main():
+    global ch, an, running
+    clock = pygame.time.Clock()
+    while running:
+        ch = 0
+        # 人类下棋
+        man_move()
+        an = g.game_result()
+        if an == 0 and ch:
+            xa, ya = g.ai_move()
+        # 判断结果
+        an = g.game_result()
+        # 刷新棋盘
+        refresh_board()
+        pygame.display.flip()
+        clock.tick(30)
+
+
+if __name__ == "__main__":
+    g = TicTacToe()  # 初始化
+    main()
